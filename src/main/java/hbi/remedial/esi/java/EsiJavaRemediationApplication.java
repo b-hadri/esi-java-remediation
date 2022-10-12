edfe3052b4aa690f94c2eccb853ffceaef497a60
b-hadri <b-hadri@users.noreply.github.com>
2022-10-12 17:59:43 +0200

    create a Spring Project with the Lombok library

---
+++ src/main/java/hbi/remedial/esi/java/EsiJavaRemediationApplication.java
@@ -0,0 +1,13 @@
+package hbi.remedial.esi.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsiJavaRemediationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsiJavaRemediationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hbi/remedial/esi/java/EsiJavaRemediationApplicationTests.java
@@ -0,0 +1,13 @@
+package hbi.remedial.esi.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsiJavaRemediationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
