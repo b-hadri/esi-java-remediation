67634daa5d866a34981d279b619eefa87944da93
b-hadri <b-hadri@users.noreply.github.com>
2022-10-13 09:18:13 +0200

    ajouter exercice sur le calcul du volume d'une sphère

--- src/main/java/hbi/remedial/esi/java/q1/evaluation/i1/dev1javl20181012/ex01/Main.java
+++ src/main/java/hbi/remedial/esi/java/q1/evaluation/i1/dev1javl20181012/ex01/Main.java
@@ -1,4 +1,25 @@
 package hbi.remedial.esi.java.q1.evaluation.i1.dev1javl20181012.ex01;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner clavier = new Scanner(System.in);
+
+        int rayon = 1;
+
+        System.out.print("Entrer un entier: ");
+        int rayonMax = clavier.nextInt();
+
+        while (rayon <= rayonMax) {
+            double volume = (4d / 3d) * Math.PI * Math.pow(rayon, 3);
+            String message = String.format("Le rayon %3d | Volume %10.2f", rayon, volume);
+            System.out.println(message);
+            rayon++;
+        }
+
+    }
+
 }
