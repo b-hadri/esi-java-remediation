85547df3a088c8812e063e203568fc32c37c13ae
b-hadri <b-hadri@users.noreply.github.com>
2022-10-13 09:02:33 +0200

    add some ESI tests

---
+++ src/main/java/hbi/remedial/esi/java/q1/evaluation/i1/dev1javl20181012/ex01/Main.java
@@ -0,0 +1,4 @@
+package hbi.remedial.esi.java.q1.evaluation.i1.dev1javl20181012.ex01;
+
+public class Main {
+}
---
+++ src/main/java/hbi/remedial/esi/java/q1/td01/ex01/Main.java
@@ -0,0 +1,12 @@
+package hbi.remedial.esi.java.q1.td01.ex01;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Slm...");
+    }
+
+}
